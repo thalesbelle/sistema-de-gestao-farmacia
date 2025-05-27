7c8552aac923c3cd974e57459db1e939dc60495d
Thales <thalesbelle0@gmail.com>
2025-05-27 17:09:27 -0300

    quarta versão.
    Co-authored-by: AryelCuriD <AryelCuriD@users.noreply.github.com>

--- pagina-inicial/pagina-inicial.js
+++ pagina-inicial/pagina-inicial.js
@@ -1,5 +1,6 @@
 const expansaoMenu = document.getElementById('iconeMenu');
 const menu = document.getElementsByClassName('menu')[0]; 
+console.log(menu);
 
 expansaoMenu.addEventListener('click', function() {
     menu.classList.toggle('expansaoMenu'); 
